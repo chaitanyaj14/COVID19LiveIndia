b7254cd682ad509b5e7f7bebe28b58934f8e0200
chaitanyaj14 <36196744+chaitanyaj14@users.noreply.github.com>
2020-04-02 13:24:43 +0530

    Second Commit

--- moh.py
+++ moh.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 
 today = date.today()
@@ -11,28 +12,36 @@ dfs = pd.read_html('https://www.mohfw.gov.in/')
 for df in dfs:
     pass
 
-df = df.drop([27])
-df = df.drop([28])
+df = df.drop([29])
+
+print('Data Table Read Completed')
 
 import json
 
-a_file = open("india.json", "r")
+a_file = open("C:\\Users\\Chaitanya\\Desktop\\CoronaVisualization\\india.json", "r")
 json_object = json.load(a_file)
 a_file.close()
 
-for i in range(0, 27):
-    for j in range(0, 27):
-        if df['Name of State / UT'][i] == json_object['data'][j]['name'] :
-            json_object['data'][j]['dead'] = df.at[i,'Death']
-            json_object['data'][j]['infected'] = df.at[i,'Total Confirmed cases *']
-            json_object['data'][j]['recovered'] = df.at[i,'Cured/Discharged/Migrated']
-            json_object['data'][j]['sick'] = str(int(df.at[i,'Total Confirmed cases *']) - (int(df.at[i,'Death']) + int(df.at[i,'Cured/Discharged/Migrated'])))
-            json_object['data'][j]['lastUpdated'] = date
+print("JSON File Opened")
 
-a_file = open("india.json", "w")
+for i in range(0, 29):
+    for j in range(0, 29):
+        if df['Name of State / UT'][i] == json_object['data'][j]['name'] :
+            json_object['data'][j]['dead'] = str(df.at[i,'Death'])
+            json_object['data'][j]['infected'] = str(df.at[i,'Total Confirmed cases (Including 51 foreign Nationals)'])
+            json_object['data'][j]['recovered'] = str(df.at[i,'Cured/Discharged/Migrated'])
+            json_object['data'][j]['sick'] = str(int(df.at[i,'Total Confirmed cases (Including 51 foreign Nationals)']) - (int(df.at[i,'Death']) + int(df.at[i,'Cured/Discharged/Migrated'])))
+            json_object['data'][j]['lastUpdated'] = date
+            break
+
+
+a_file = open("C:\\Users\\Chaitanya\\Desktop\\CoronaVisualization\\india.json", "w")
 json.dump(json_object, a_file)
 a_file.close()
 
+
 df2 = pd.DataFrame(df)
-df2 = df2.rename(columns={"Name of State / UT": "name", "Total Confirmed cases *": "infected"})
+df2 = df2.rename(columns={"Name of State / UT": "name", "Total Confirmed cases (Including 51 foreign Nationals)": "infected"})
 df2.to_json(r'piechart.json',orient='records')
+
+print("JSON Updated")
